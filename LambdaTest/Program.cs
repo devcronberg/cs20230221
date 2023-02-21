e1e76bd851f909940c7da6d308b304ca60e3ccbd
Your Name <you@example.com>
2023-02-21 16:00:31 +0100

    færdig

---
+++ LambdaTest/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace LambdaTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Action test1 = () => { Console.WriteLine("I test1"); };
+            Action test1 = () => Console.WriteLine("I test1"); 
+            //test1.Invoke();
+            test1();
+
+            //Action<int> test2 = (int a) => { Console.WriteLine($"I test2 med {a}"); };
+            //Action<int> test2 = (a) => { Console.WriteLine($"I test2 med {a}"); };
+            //Action<int> test2 = a => { Console.WriteLine($"I test2 med {a}"); };
+            Action<int> test2 = a => Console.WriteLine($"I test2 med {a}"); 
+            test2(1);
+
+            //Action<int, double> test3 = (int a, double b) => { Console.WriteLine($"I Test3 med {a} og {b}"); };
+            //Action<int, double> test3 = (a, b) => { Console.WriteLine($"I Test3 med {a} og {b}"); };
+            Action<int, double> test3 = (a, b) => Console.WriteLine($"I Test3 med {a} og {b}"); 
+            test3(1, 5.78);
+
+            //Action<int, double> test3a = (int a, double b) => { 
+            //    Console.WriteLine($"I Test3 med {a} og {b}");
+            //    Console.Beep();
+            //    Console.WriteLine($"I Test3 med {a} og {b}");
+            //};
+            Action<int, double> test3a = (a, b) => {
+                Console.WriteLine($"I Test3 med {a} og {b}");
+                Console.Beep();
+                Console.WriteLine($"I Test3 med {a} og {b}");
+            };
+            test3a(1, 5.78);
+
+
+            //Func<int, int> test4 = (int a) => { return a + 1; };
+            //Func<int, int> test4 = (a) => { return a + 1; };
+            //Func<int, int> test4 = a => { return a + 1; };
+            Func<int, int> test4 = a => a + 1; 
+            Console.WriteLine(test4(1));
+            
+            Func<int, int, int> test5 = (a, b) => a + b;
+            Console.WriteLine(test5(1,1));
+
+            //Func<int, bool> test6a = (int a) => { return a < 10; };
+            Func<int, bool> test6a = a => a < 10; 
+            Predicate<int> test6b = a => a < 10;
+            Console.WriteLine(test6a(1));
+            Console.WriteLine(test6b(1));
+
+
+            Console.WriteLine();
+            Test1();
+            Test2(1);
+            Test3(1, 5.76);
+            Test3a(1, 5.76);
+            Console.WriteLine(Test4(1));
+            Console.WriteLine(Test5(1,1));
+            Console.WriteLine(Test6(1));
+
+            void Test1()
+            {
+                Console.WriteLine("I Test1");
+            }
+
+            void Test2(int a)
+            {
+                Console.WriteLine($"I Test2 med {a}");
+            }
+            
+            void Test3(int a, double b)
+            {
+                Console.WriteLine($"I Test3 med {a} og {b}");
+            }
+
+            void Test3a(int a, double b)
+            {
+                Console.WriteLine($"I Test3 med {a} og {b}");
+                Console.Beep();
+                Console.WriteLine($"I Test3 med {a} og {b}");
+            }
+
+            int Test4(int a)
+            {
+                return a + 1;
+            }
+
+            int Test5(int a, int b)
+            {
+                return a + b;
+            }
+
+            bool Test6(int a)
+            {
+                return a < 10;
+            }
+        }
+    }
+}
---
+++ LinqTest/Program.cs
@@ -0,0 +1,32 @@
+﻿//using System.Linq;
+
+namespace LinqTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var mappe = new System.IO.DirectoryInfo(@"C:\Users\H-828-INS\source\repos");
+            var filer = mappe.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
+            Console.WriteLine($"Har fundet {filer.Length} i {mappe.Name}");
+
+            var res1 = from fil in filer where 
+                        fil.Length > 10000 orderby fil.Name select fil;
+
+            //foreach (var item in res1)
+            //{
+            //    Console.WriteLine(item.Name + " "+ item.Length);
+            //}
+            int l = 10000;
+            var res2 = filer.Where(i => i.Length > l).ToList();
+            Console.WriteLine(res2.FirstOrDefault()?.Name);
+
+        }
+    }
+
+    class MinFileInfo
+    {
+        public string Navn { get; set; }
+        public long Længde { get; set; }
+    }
+}
